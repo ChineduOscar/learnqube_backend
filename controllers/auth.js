7ecb632c30463b14c49fee9c0f7365ceed5e8a96
ChineduOscar <chinedunnadoziepatrick@gmail.com>
2025-02-20 04:12:43 +0100

    working with cookies again

--- controllers/auth.js
+++ controllers/auth.js
@@ -44,17 +44,32 @@ const googleCallback = (req, res, next) => {
   passport.authenticate('google', (err, user) => {
     if (err) return next(err);
     if (!user) return res.redirect(`${process.env.FRONTEND_URL}/login`);
+
+    const token = user.createJWT();
+
     res.cookie("userToken", token, {
       httpOnly: true, // Prevents JavaScript access
       secure: true, // Ensures it's only sent over HTTPS
       sameSite: "None", // Allows cross-origin requests
       domain: ".onrender.com", // Adjust based on your domain
     });
-
+    
     res.redirect(`/protected`);
   })(req, res, next);
 };
 
+const authenticated = (req, res) => {
+  const token = user.createJWT();
+
+    res.status(StatusCodes.OK).json({
+      user: {
+        name: user.name,
+        role: user.role,
+      },
+      token,
+    });
+
+};
 
 const logout = (req, res) => {
   req.logout();
